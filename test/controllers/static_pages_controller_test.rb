7aaee112a14d3ade981a77ca762837fd397ad023
kevin <kevinochieng548@gmail.com>
2016-06-21 15:48:47 +0300

    add test for title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,28 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+    common_title = " | Our App"
+  
+
   test "should get home" do
     get root_url
     assert_response :success
-    assert_select "title", "Home | Our App"
+    assert_select "title", "Home#{common_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Our App"
+    assert_select "title", "Help#{common_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Our App"
+    assert_select "title", "About#{common_title}"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact | Our App"
+    assert_select "title", "Contact#{common_title}"
   end
 
   
